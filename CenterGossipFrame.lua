3fd7751247e31ae4b1daa2c2aeb13c168b3717e6
Adriano Castro <adrianostanley@gmail.com>
2024-07-18 16:24:47 -0300

    A complete refactor of the centralization process

--- CenterGossipFrame.lua
+++ CenterGossipFrame.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: duplicate-set-field
+
 CenterGossipFrame = StormwindLibrary_v1_9_0.new({
   colors = {
     primary = 'ED5859'
@@ -6,13 +8,32 @@ CenterGossipFrame = StormwindLibrary_v1_9_0.new({
 })
 
 --[[
-Determines whether a frame can be centralized or not.
+Apply a listener to a frame.
+
+The listener will be responsible for detecting when the frame is updated and
+centralizing it if necessary. When the frame is already centralized, no changes
+will be made, so the listener will not be invoked repeatedly.
+
+@param gameFrame The frame to be listened to
 ]]
-function CenterGossipFrame:canBeCentralized(frame)
+function CenterGossipFrame:applyListener(gameFrame)
+  if not self:canBeCentralized(gameFrame) then return end
+
+  gameFrame:HookScript('OnUpdate', function()
+    CenterGossipFrame:maybeCentralizeFrame(gameFrame)
+  end)
+end
+
+--[[
+Determines whether a frame can be centralized or not.
+
+@param gameFrame The frame to be checked
+]]
+function CenterGossipFrame:canBeCentralized(gameFrame)
   return
-    frame ~= nil and
-    frame.ClearAllPoints ~= nil and
-    frame.SetPoint ~= nil
+    gameFrame ~= nil and
+    gameFrame.ClearAllPoints ~= nil and
+    gameFrame.SetPoint ~= nil
 end
 
 --[[
@@ -21,40 +42,42 @@ Positions a frame in the center of the screen.
 This method was designed to be used with the GossipFrame and QuestFrame. It
 will probably work with many other frames, but must be well tested if the
 addon covers other frames in the future.
+
+@param gameFrame The frame to be centralized
 ]]
-function CenterGossipFrame:centralizeFrame(frame)
-  frame:ClearAllPoints()
-  frame:SetPoint('CENTER', UIParent, 'CENTER', 0, 0)
+function CenterGossipFrame:centralizeFrame(gameFrame)
+  gameFrame:ClearAllPoints()
+  gameFrame:SetPoint('CENTER', UIParent, 'CENTER', 0, 0)
+end
+
+--[[
+Determines whether a frame is already centered or not.
+
+@param gameFrame The frame to be checked
+]]
+function CenterGossipFrame:isFrameCentered(frame)
+  local point, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
+
+  return
+    point == 'CENTER' and
+    relativeTo == UIParent and
+    relativePoint == 'CENTER' and
+    offsetX == 0 and
+    offsetY == 0
 end
 
 --[[
 May centralize a frame if it can be centralized.
+
+@param frame The frame to be centralized
 ]]
 function CenterGossipFrame:maybeCentralizeFrame(frame)
-  if not self:canBeCentralized(frame) then return end
-
-  self:centralizeFrame(frame)
-
-  -- this line solves an edge case where the frame is not centralized due
-  -- to race conditions between events and the default frame positioning
-  C_Timer.After(0.001, function () self:centralizeFrame(frame) end)
+  if not self:isFrameCentered(frame) then
+    self:centralizeFrame(frame)
+  end
 end
 
--- event that triggers when the player interacts with things that can open
--- centralizable frames
-CenterGossipFrame.events:listenOriginal('PLAYER_INTERACTION_MANAGER_FRAME_SHOW', function ()
-  CenterGossipFrame:maybeCentralizeFrame(GossipFrame)
-  CenterGossipFrame:maybeCentralizeFrame(MerchantFrame)
-end)
-
--- events that trigger when the quest frame is shown
-for _, event in ipairs({'QUEST_DETAIL', 'QUEST_COMPLETE', 'QUEST_PROGRESS'}) do
-  CenterGossipFrame.events:listenOriginal(event, function ()
-    CenterGossipFrame:maybeCentralizeFrame(QuestFrame)
-  end)
-end
-
--- event that triggers when the taximap frame is shown
-CenterGossipFrame.events:listenOriginal('TAXIMAP_OPENED', function ()
-  CenterGossipFrame:maybeCentralizeFrame(TaxiFrame)
-end)
+CenterGossipFrame:applyListener(GossipFrame)
+CenterGossipFrame:applyListener(MerchantFrame)
+CenterGossipFrame:applyListener(QuestFrame)
+CenterGossipFrame:applyListener(TaxiFrame)
--- tests/CenterGossipFrameTest.lua
+++ tests/CenterGossipFrameTest.lua
@@ -1,4 +1,48 @@
+---@diagnostic disable: duplicate-set-field
+
 TestCenterGossipFrame = BaseTestClass:new()
+    -- @covers CenterGossipFrame:applyListener()
+    function TestCenterGossipFrame:testApplyListener()
+        CenterGossipFrame.canBeCentralized = function() return true end
+        CenterGossipFrame.maybeCentralizeFrame = function(self, gameFrame)
+            CenterGossipFrame.centralizeInvoked = true
+            gameFrame.centralized = true
+        end
+
+        local frame = {
+            HookScript = function(self, event, callback)
+                self.event = event
+                self.callback = callback
+            end
+        }
+
+        CenterGossipFrame:applyListener(frame)
+
+        lu.assertEquals('OnUpdate', frame.event)
+        lu.assertIsNil(CenterGossipFrame.centralizeInvoked)
+        lu.assertIsNil(frame.centralized)
+
+        frame.callback()
+
+        lu.assertTrue(CenterGossipFrame.centralizeInvoked)
+        lu.assertTrue(frame.centralized)
+    end
+
+    -- @covers CenterGossipFrame:applyListener()
+    function TestCenterGossipFrame:testApplyListenerWhenCantBeCentralized()
+        CenterGossipFrame.canBeCentralized = function() return false end
+        
+        local frame = {
+            HookScript = function(self)
+                self.scriptHooked = true
+            end
+        }
+
+        CenterGossipFrame:applyListener(frame)
+
+        lu.assertIsNil(frame.scriptHooked)
+    end    
+
     -- @covers CenterGossipFrame:canBeCentralized
     function TestCenterGossipFrame:testCanBeCentralized()
         local function execution(frame, expected)
@@ -35,13 +79,37 @@ TestCenterGossipFrame = BaseTestClass:new()
         }, frame.points)
     end
 
+    -- @covers CenterGossipFrame:isFrameCentered()
+    function TestCenterGossipFrame:testIsFrameCentered()
+        local function execution(point, relativeTo, relativePoint, offsetX, offsetY, expected)
+            local frame = {
+                GetPoint = function()
+                    return point, relativeTo, relativePoint, offsetX, offsetY
+                end
+            }
+
+            lu.assertEquals(expected, CenterGossipFrame:isFrameCentered(frame))
+        end
+
+        -- frame is not centered
+        execution('TOPLEFT', UIParent, 'TOPLEFT', 0, 0, false)
+
+        -- frame is centered
+        execution('CENTER', UIParent, 'CENTER', 0, 0, true)
+    end
+
+    -- @covers CenterGossipFrame
+    function TestCenterGossipFrame:testMainAddonFile()
+        lu.assertNotIsNil(CenterGossipFrame)
+    end
+
     -- @covers CenterGossipFrame:maybeCentralizeFrame()
     function TestCenterGossipFrame:testMaybeCentralizeFrame()
-        local function execution(canBeCentralized, shouldInvokeCentralizeFrame)
+        local function execution(isFrameCentered, shouldInvokeCentralizeFrame)
             local frame = CreateFrame()
             frame.centralizeInvoked = false
 
-            CenterGossipFrame.canBeCentralized = function() return canBeCentralized end
+            CenterGossipFrame.isFrameCentered = function() return isFrameCentered end
             CenterGossipFrame.centralizeFrame = function() frame.centralizeInvoked = true end
 
             CenterGossipFrame:maybeCentralizeFrame(frame)
@@ -49,15 +117,10 @@ TestCenterGossipFrame = BaseTestClass:new()
             lu.assertEquals(shouldInvokeCentralizeFrame, frame.centralizeInvoked)
         end
 
-        -- frame cannot be centralized
-        execution(false, false)
+        -- frame is centered
+        execution(true, false)
 
-        -- frame can be centralized
-        execution(true, true)
-    end
-
-    -- @covers CenterGossipFrame
-    function TestCenterGossipFrame:testMainAddonFile()
-        lu.assertNotIsNil(CenterGossipFrame)
+        -- frame is not centered
+        execution(false, true)
     end
 -- end of MultiTargetsTest
