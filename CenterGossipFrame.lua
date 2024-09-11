52ae9743da9a1d4a3d879ca2dc3cf1cd24432651
Adriano Castro <adrianostanley@gmail.com>
2024-09-11 15:20:18 -0300

    Register covered frames

--- CenterGossipFrame.lua
+++ CenterGossipFrame.lua
@@ -8,16 +8,21 @@ CenterGossipFrame = StormwindLibrary_v1_13_0.new({
     version = '1.3.0',
 })
 
-
-
--- CenterGossipFrame:applyListener(GossipFrame)
--- CenterGossipFrame:applyListener(MerchantFrame)
--- CenterGossipFrame:applyListener(QuestFrame)
--- CenterGossipFrame:applyListener(TaxiFrame)
-
 local events = CenterGossipFrame.events
 
 events:listen(events.EVENT_NAME_PLAYER_LOGIN, function()
     -- initializes the TSM integration
     CenterGossipFrame.tsmIntegration = CenterGossipFrame:new('CenterGossipFrame/TradeSkillMasterIntegration')
+
+    local coveredFrames = {
+        CenterGossipFrame:new('CenterGossipFrame/GenericCoveredFrame', GossipFrame),
+        CenterGossipFrame:new('CenterGossipFrame/GenericCoveredFrame', QuestFrame),
+        CenterGossipFrame:new('CenterGossipFrame/GenericCoveredFrame', TaxiFrame),
+        CenterGossipFrame:new('CenterGossipFrame/MerchantCoveredFrame'),
+        CenterGossipFrame:new('CenterGossipFrame/ClassTrainerCoveredFrame'),
+    }
+
+    CenterGossipFrame.arr:each(coveredFrames, function(coveredFrame)
+        coveredFrame:register()
+    end)
 end)
